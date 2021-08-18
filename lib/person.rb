3beba6dc31b20eea127fa6078a00c2a37f8d14aa
Milan <milanm1120@gmail.com>
2021-08-18 10:41:58 -0400

    commit tree 538925cb121227685303942d277865369fda269d

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
 # your code goes here
+class Person
+
+    attr_reader :name
+    attr_accessor :bank_account, :happiness
+
+    def initialize(name, bank_account = 25, happiness = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+    end
+
+    # def happiness
+    #     @happiness <= 10
+    # end
+
+end
